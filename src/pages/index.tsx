27d4b22e759c00d6dd670a3d5dcc0fbdebb54dff
Rui Ribeiro <ruiandreribeiro@protonmail.com>
2022-08-16 19:23:45 +0100

    Move files around and clean up

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Header from "../components/Header";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Header />
+      <p>Hello!</p>
+    </>
+  );
+};
+
+export default Home;
